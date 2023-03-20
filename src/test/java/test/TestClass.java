a54db6905081957a30f0cd27d3bcbb81a864d949
aniltej <aniltej96@gmail.com>
2023-03-20 07:39:06 +0530

    vv

--- src/test/java/test/TestClass.java
+++ src/test/java/test/TestClass.java
@@ -1,10 +1,26 @@
 package test;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestClass {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Anil. Code Pused to GIT on 15th Feb 2:30 PM");
-
+	@Test
+	public void methodOne()
+	{
+	
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		RemoteWebDriver driver = new ChromeDriver(co);
+		driver.get("https://automationexercise.com/signup");
+		Assert.assertTrue(true);
+		driver.close();
 	}
 
 }
